a3e24a9bc70f8c200388e8ac96a36255e36fbf31
Nikola <1388673+jurkovic-nikola@users.noreply.github.com>
2024-09-22 12:02:49 +0200

    simple upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+CURRENT_USER=$SUDO_USER
+PRODUCT="OpenLinkHub"
+PRODUCT_FILE="/opt/$PRODUCT/$PRODUCT"
+
+if [ ! -f $PRODUCT ]; then
+  echo "No binary file. Exit"
+  exit 0
+fi
+
+if [ -f $PRODUCT_FILE ]; then
+  echo "Performing upgrade..."
+  sudo systemctl stop $PRODUCT
+  cp OpenLinkHub "/opt/$PRODUCT/"
+  cp -r web/ "/opt/$PRODUCT/"
+  cp -r static/ "/opt/$PRODUCT/"
+  cp config.json "/opt/$PRODUCT/"
+  chmod -R 755 /opt/$PRODUCT
+  chown -R "$CURRENT_USER":root /opt/$PRODUCT
+  sudo systemctl start $PRODUCT
+  exit 0
+fi

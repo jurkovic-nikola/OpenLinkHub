472c01b9509a4f29a7b4a33af72cd002e5fdef10
jurkovic-nikola <1388673+jurkovic-nikola@users.noreply.github.com>
2025-02-23 22:46:22 +0100

    template cleanup

--- src/templates/templates.go
+++ src/templates/templates.go
@@ -1,6 +1,7 @@
 package templates
 
 import (
+	"OpenLinkHub/src/common"
 	"OpenLinkHub/src/config"
 	"OpenLinkHub/src/dashboard"
 	"OpenLinkHub/src/devices"
@@ -9,7 +10,9 @@ import (
 	"OpenLinkHub/src/scheduler"
 	"OpenLinkHub/src/temperatures"
 	"OpenLinkHub/src/version"
+	"fmt"
 	"html/template"
+	"os"
 )
 
 var (
@@ -40,75 +43,25 @@ type Web struct {
 
 // Init will parse all templates
 func Init() {
-	tpl, err := template.ParseFiles(
-		"web/docs.html",
-		"web/index.html",
-		"web/lsh.html",
-		"web/cc.html",
-		"web/ccxt.html",
-		"web/elite.html",
-		"web/lncore.html",
-		"web/lnpro.html",
-		"web/cpro.html",
-		"web/xc7.html",
-		"web/memory.html",
-		"web/k65pm.html",
-		"web/k65plus.html",
-		"web/k65plusW.html",
-		"web/k70core.html",
-		"web/k70coretkl.html",
-		"web/k70coretklWU.html",
-		"web/k70coretklW.html",
-		"web/k70pro.html",
-		"web/k70protkl.html",
-		"web/k70mk2.html",
-		"web/k55core.html",
-		"web/k100.html",
-		"web/k100air.html",
-		"web/k100airW.html",
-		"web/st100.html",
-		"web/mm700.html",
-		"web/lt100.html",
-		"web/psuhid.html",
-		"web/katarpro.html",
-		"web/katarproxt.html",
-		"web/ironclaw.html",
-		"web/ironclawW.html",
-		"web/ironclawWU.html",
-		"web/slipstream.html",
-		"web/nightsabreW.html",
-		"web/nightsabreWU.html",
-		"web/scimitar.html",
-		"web/scimitarWU.html",
-		"web/scimitarW.html",
-		"web/m55.html",
-		"web/m55W.html",
-		"web/m55rgbpro.html",
-		"web/katarproW.html",
-		"web/darkcorergbproseW.html",
-		"web/darkcorergbproseWU.html",
-		"web/darkcorergbproW.html",
-		"web/darkcorergbproWU.html",
-		"web/m75.html",
-		"web/m75W.html",
-		"web/m75WU.html",
-		"web/m65rgbultra.html",
-		"web/harpoonrgbpro.html",
-		"web/harpoonWU.html",
-		"web/harpoonW.html",
-		"web/virtuosorgbXTWU.html",
-		"web/virtuosorgbXTW.html",
-		"web/darkstarWU.html",
-		"web/darkstarW.html",
-		"web/k70rgbtklcs.html",
-		"web/rgb.html",
-		"web/temperature.html",
-		"web/scheduler.html",
-		"web/navigation.html",
-		"web/footer.html",
-		"web/header.html",
-		"web/404.html",
-	)
+	var templateList []string
+	htmlDirectory := config.GetConfig().ConfigPath + "/web/"
+	files, err := os.ReadDir(htmlDirectory)
+	if err != nil {
+		logger.Log(logger.Fields{"error": err, "path": htmlDirectory}).Fatal("Unable to read content of a html directory")
+	}
+
+	for _, fi := range files {
+		templateFile := fmt.Sprintf("%s%s", htmlDirectory, fi.Name())
+
+		// Check if filename has .html extension
+		if !common.IsValidExtension(templateFile, ".html") {
+			continue
+		}
+
+		templateList = append(templateList, templateFile)
+	}
+
+	tpl, err := template.ParseFiles(templateList...)
 	if err != nil {
 		logger.Log(logger.Fields{"error": err}).Fatal("Failed to load templates")
 	}

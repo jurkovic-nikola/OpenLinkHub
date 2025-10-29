3e505bcb2cd24f84e5fea11ff62ce0b7c1492c82
jurkovic-nikola <1388673+jurkovic-nikola@users.noreply.github.com>
2025-10-29 20:45:21 +0100

    immutable installation fixups

--- install-immutable.sh
+++ install-immutable.sh
@@ -7,6 +7,8 @@ PERMISSION_FILE="99-openlinkhub.rules"
 PERMISSION_DIR="/etc/udev/rules.d/"
 SEARCH_FOR="openlinkhub"
 USER_TO_CHECK=$USER
+SEARCH_FOR_GROUP='OWNER="openlinkhub"'
+REPLACE_WITH='GROUP="openlinkhub"'
 
 if [ ! -f $PRODUCT ]; then
   echo "No binary file. Exit"
@@ -19,6 +21,26 @@ if id "$USER_TO_CHECK" &>/dev/null; then
 else
     echo "Username $USER_TO_CHECK not found. Exit"
     exit 0
+fi
+
+if getent group "$SEARCH_FOR" >/dev/null; then
+    echo "Group '$SEARCH_FOR' already exists, skipping creation."
+else
+    # Loop through system GIDs to find a free one
+    for gid in $(seq 999 -1 900); do
+        if ! getent group "$gid" >/dev/null; then
+            sudo groupadd -g "$gid" "$SEARCH_FOR"
+            echo "Created group '$SEARCH_FOR' with GID $gid"
+            break
+        fi
+    done
+fi
+
+if id -nG "$USER_TO_CHECK" | grep -qw "$SEARCH_FOR"; then
+    echo "$USER_TO_CHECK is already in $SEARCH_FOR"
+else
+    sudo usermod -aG "$SEARCH_FOR" "$USER_TO_CHECK"
+    echo "Added $USER_TO_CHECK to $SEARCH_FOR"
 fi
 
 SYSTEMD_FILE="/home/$USER_TO_CHECK/.config/systemd/user/OpenLinkHub.service"
@@ -47,8 +69,9 @@ if [ -f "$SYSTEMD_FILE" ]; then
 fi
 
 echo "Copy device permission file $PERMISSION_FILE to $PERMISSION_DIR"
-sed -i -e "s/$SEARCH_FOR/$USER_TO_CHECK/g" $PERMISSION_FILE
+sed -i -e "s/$SEARCH_FOR_GROUP/$REPLACE_WITH/g" $PERMISSION_FILE
 sudo cp "$CURRENT_DIR/$PERMISSION_FILE" $PERMISSION_DIR
+sudo chmod -x $PERMISSION_DIR
 
 echo "Reloading permissions..."
 sudo udevadm control --reload-rules

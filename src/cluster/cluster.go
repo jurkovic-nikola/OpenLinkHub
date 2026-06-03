3a4a4a14b7fb8823792808db48f537099685e406
Alaric07 <EricWeiss07@proton.me>
2026-06-03 13:42:28 -0600

    fix(cluster): add off animation to rgbProfileUpgrade to prevent UI failure

--- src/cluster/cluster.go
+++ src/cluster/cluster.go
@@ -23,7 +23,7 @@ var (
 	pwd                   = ""
 	d                     *Device
 	deviceRefreshInterval = 1000
-	rgbProfileUpgrade     = []string{"arc", "gradient", "pastelrainbow", "pastelspiralrainbow", "rain"}
+	rgbProfileUpgrade     = []string{"arc", "gradient", "pastelrainbow", "pastelspiralrainbow", "rain", "off"}
 )
 
 type DeviceProfile struct {

ae4f4ee8fd541c47988329f621acc4563d59d610
Alaric07 <EricWeiss07@proton.me>
2026-07-28 09:21:43 -0600

    Close metadata files on decode errors

--- src/devices/ccxt/ccxt.go
+++ src/devices/ccxt/ccxt.go
@@ -468,13 +468,14 @@ func (d *Device) loadExternalDevices() {
 			logger.Log(logger.Fields{"error": err, "serial": d.Serial, "location": externalDevicesFile}).Warn("Unable to load external devices metadata")
 			return
 		}
+		defer func() {
+			if err = file.Close(); err != nil {
+				logger.Log(logger.Fields{"location": externalDevicesFile, "serial": d.Serial}).Warn("Failed to close external devices metadata")
+			}
+		}()
 		if err = json.NewDecoder(file).Decode(&d.ExternalLedDevice); err != nil {
 			logger.Log(logger.Fields{"error": err, "serial": d.Serial, "location": externalDevicesFile}).Warn("Unable to decode external devices metadata")
 			return
-		}
-		err = file.Close()
-		if err != nil {
-			logger.Log(logger.Fields{"location": externalDevicesFile, "serial": d.Serial}).Warn("Failed to close external devices metadata")
 		}
 	} else {
 		logger.Log(logger.Fields{"serial": d.Serial, "location": externalDevicesFile}).Warn("Unable to load external devices metadata")
